65daf47f07a6f5d37f5f565c4dabe9d180e336b7
Pham Van Khang <phamvankhang.tvi@gmail.com>
2023-06-06 23:40:23 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+![Tuturial TODO List REST API Golang](https://200lab-blog.imgix.net/2022/05/backend_golang-03.jpg?auto=format,compress&w=1000)
+
+# TODO REST API Service
+
+This is a very simple Golang REST API service. My full tutorial article (in Vietnamese) has been published at https://200lab.io/blog/lap-trinh-rest-api-todo-list-voi-golang/
+
+# How to run this service
+
+- Make sure you've already had Golang & MySQL installed on your local machine.
+- Clone this repository
+- Use Terminal, change directory to your folder an run command `MYSQL_CONNECTION='YOUR_MYSQL_CONNECTION_STRING' go run main.go`
+- If everything is OK. A service is running and listen at port 8080.
+
+nodemon --signal SIGTERM --exec "go run main.go"
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+	todotrpt "first-app/module/item/transport"
+)
+
+func main() {
+	// Checking that an environment variable is present or not.
+	// Load the environment variable
+
+	godotenv.Load(".env")
+
+	mysqlConnStr, ok := os.LookupEnv("MYSQL_CONNECTION")
+
+	if !ok {
+		log.Fatalln("Missing MySQL connection string.")
+	}
+
+	dsn := mysqlConnStr
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln("Cannot connect to MySQL:", err)
+	}
+
+	router := gin.Default()
+
+	v1 := router.Group("/v1")
+	{
+		v1.POST("/items", todotrpt.HandleCreateItem(db))    // create item
+		v1.GET("/items", todotrpt.HandleListItem(db))       // list items
+		v1.GET("/items/:id", todotrpt.HandleFindAnItem(db)) // get an item by ID
+		// v1.PUT("/items/:id", todotrpt.HandleUpdateAnItem(db)) // edit an item by ID
+		// v1.DELETE("/items/:id", todotrpt.HandleDeleteAnItem(db)) // delete an item by ID
+	}
+
+	router.Run()
+}
